689dd94d5cc7735cb7e9977a556874e705a358cf
Mikhail Mustakimov <mikhail@mustakimov.ru>
2018-12-28 18:56:54 +0800

    Working with multiple enters and exits

--- main.cpp
+++ main.cpp
@@ -21,28 +21,49 @@ int main(int argc, char **argv) {
 
     BMP file(inputFilename);
 
+    std::cout << "Generating maze..." << std::endl;
     RGBPixel startColor(36, 28, 237);
     RGBPixel endColor(76, 177, 34);
     MazeWithPoints maze = *MazeUtils().generateMazeFromBmp(file, startColor, endColor);
+    std::cout << "End of maze generation" << std::endl;
 
     PathFinder pathFinder(maze);
 
+    std::cout << "Enter points:" << std::endl;
     for (auto enter : maze.getEnterPoints()) {
-        std::cout << "Enter: " << enter;
+        std::cout << enter << std::endl;
     }
+    std::cout << "Exit points:" << std::endl;
     for (auto ending : maze.getExitPoints()) {
-        std::cout << "End: " << ending;
+        std::cout << ending << std::endl;
     }
     std::cout.flush();
 
-    std::vector<Point> result = pathFinder.wave(maze.getEnterPoints()[0], maze.getExitPoints()[0]);
+    std::vector<std::vector<Point>> results;
+
+    for (auto enter : maze.getEnterPoints()) {
+        for (auto exit : maze.getExitPoints()) {
+            results.push_back(pathFinder.wave(enter, exit));
+        }
+    }
+
+    long shortestResult = 0;
+    for (int i = 1; i < results.size(); i++) {
+        if (results[i].size() < results[shortestResult].size()) {
+            shortestResult = i;
+        }
+    }
+
+    std::vector<Point> result = results[shortestResult];
     RGBPixel blue;
     blue.blue = 255;
     blue.red = 255;
     blue.green = 0;
 
     if (result.empty()) {
-        std::cout << "No path found...";
+        std::cout << "No path found..." << std::endl;
+    } else {
+        std::cout << "Shortest path: " << result.size() << std::endl;
     }
 
     for (auto elem : result) {
